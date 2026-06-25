8d265ef43f5977faf827668431124a2a3800b747
Faizan-Zaheer6 <faizan.zaheer6@gmail.com>
2026-06-25 18:58:01 +0500

    added

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 import os
 import sys
+# pyrefly: ignore [missing-import]
 from mangum import Mangum
-
 # Python ko project ka root path batane ke liye
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
