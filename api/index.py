7920104b15eb5d2deb5adeef483d4f511afe103e
Faizan-Zaheer6 <faizan.zaheer6@gmail.com>
2026-06-25 18:53:09 +0500

    added complete project files and backend modules

--- api/index.py
+++ api/index.py
@@ -1,4 +1,9 @@
+import os
+import sys
 from mangum import Mangum
+
+# Python ko project ka root path batane ke liye
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app.main import app
 
